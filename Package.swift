fe5545e51699a040650a70488d8d21ac89aa171d
Darren Ehlers <me@darrenehlers.com>
2020-02-29 23:23:10 -0600

    Readded AlamofireImage package

--- Package.swift
+++ Package.swift
@@ -20,6 +20,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.2"),
+        .package(url: "https://github.com/Alamofire/AlamofireImage.git", from: "4.0.0-beta.6"),
         .package(url: "https://github.com/MarioIannotta/AtomicSwift.git", from: "1.0.0"),
         .package(url: "https://github.com/DoubleNode/DNSAppCore.git", from: "1.0.4"),
         .package(url: "https://github.com/JanGorman/Hippolyte.git", from: "1.0.0"),
@@ -29,7 +30,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "DNSNetwork",
-            dependencies: ["Alamofire", "AtomicSwift", "DNSAppCore"]),
+            dependencies: ["Alamofire", "AlamofireImage", "AtomicSwift", "DNSAppCore"]),
         .testTarget(
             name: "DNSNetworkTests",
             dependencies: ["DNSNetwork", "Hippolyte"]),
