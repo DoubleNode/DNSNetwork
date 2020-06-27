ecf22a15d9885e68203645c852ba4d6fa49ac48c
Darren Ehlers <me@darrenehlers.com>
2020-06-27 17:20:38 -0500

    Updated package target to dynamic

--- Package.swift
+++ Package.swift
@@ -21,6 +21,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "DNSNetwork",
+            type: .dynamic,
             targets: ["DNSNetwork"]),
     ],
     dependencies: [
